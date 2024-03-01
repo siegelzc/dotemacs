9199390a7b9dff3ff959d0d3db36e3e95dfb79bf
Zachary Siegel <siegelzc@gmail.com>
2024-02-29 21:28:33 -0600

    mkdir ~/.emacs.d

--- link.sh
+++ link.sh
@@ -4,4 +4,5 @@ echo 'Backing up ~/.emacs.d/init.el to ~/.emacs.d/init.el.bak'
 mv ~/.emacs.d/init.el ~/.emacs.d/init.el.bak
 
 echo 'Overwriting ~/.emacs.d/init.el'
+mkdir -p ~/.emacs.d
 ln -s $(realpath init.el) ~/.emacs.d
